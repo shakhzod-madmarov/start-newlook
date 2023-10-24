33663d338520a524aeff42ac5c9a2c2c4f2eca8f
SHAKHZOD <113131580+shakhzod-madmarov@users.noreply.github.com>
2023-10-25 01:16:21 +0500

    added pages

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const navMenuBtn = document.querySelector(".navMenuBtn");
 const navList = document.querySelector(".navList");
 const body = document.body;
+const navLinks = document.querySelectorAll(".navLink");
+const windowPathName = window.location.pathname;
+
+navLinks.forEach((navLink) => {
+  const navLinkPathName = new URL(navLink.href).pathname;
+
+  if (
+    windowPathName === navLinkPathName ||
+    (windowPathName === "/index.html" && navLinkPathName === "./pages/")
+  ) {
+    navLink.classList.add("active");
+  }
+});
 
 navMenuBtn.addEventListener("click", function () {
   navList.classList.toggle("active");
