6345cc752b47030ab5dd69af80f9bc542e94b8f0
SHAKHZOD <113131580+shakhzod-madmarov@users.noreply.github.com>
2023-10-24 18:54:50 +0500

    updated css and html

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const navMenuBtn = document.querySelector(".navMenuBtn");
 const navList = document.querySelector(".navList");
+const body = document.body;
 
 navMenuBtn.addEventListener("click", function () {
   navList.classList.toggle("active");
+
+  if (navList.classList.contains("active")) {
+    body.classList.add("noScroll");
+  } else {
+    body.classList.remove("noScroll");
+  }
 });
